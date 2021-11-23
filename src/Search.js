a8c6100dd1b0ef7e762dfddb913a53d46b4a7930
Marina <mpyak93@gmail.com>
2021-11-23 13:11:03 -0500

    Completed API integration in the "Search" component

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-
 import "./Search.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTemperatureLow,
+  faTint,
+  faWind,
+} from "@fortawesome/free-solid-svg-icons";
 export default function Search() {
   const [city, setCity] = useState("");
   const [loaded, setLoaded] = useState(false);
@@ -16,7 +21,7 @@ export default function Search() {
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
       low: response.data.main.temp_min,
-      cityName: response.data.name,
+      city: response.data.name,
     });
   }
 
@@ -41,6 +46,7 @@ export default function Search() {
             type="search"
             placeholder="Enter a city..."
             onChange={updateCity}
+            autoFocus
           />
         </div>
         <div className="col-4">
@@ -55,27 +61,83 @@ export default function Search() {
     return (
       <div>
         {form}
-        <div className="col-5">
-          <h5 className="card-title" id="currentCity">
-            {weather.cityName}
-          </h5>
-          <div className="row">
-            <div className="col-3 weather-temperature">
-              <img
-                src={weather.icon}
-                className="card-img-top today-icon"
-                id="current-day-icon"
-                alt={weather.description}
-              />
+        <div className="row top-row">
+          <div className="col-6">
+            <div className="card">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-5">
+                    <h5 className="card-title" id="currentCity">
+                      {weather.city}
+                    </h5>
+                    <h6
+                      className="card-subtitle mb-2 text-muted"
+                      id="currentDayCard"
+                    >
+                      Thursday 3:30 pm
+                    </h6>
+                    <div className="row">
+                      <div className="col-7 weather-temperature">
+                        <img
+                          src={weather.icon}
+                          className="card-img-top today-icon"
+                          id="current-day-icon"
+                          alt="weather-icon"
+                        />
 
-              <h3 className="today-temperature">
-                {Math.round(weather.temperature)}
-              </h3>
-              <span className="temperature-unit">
-                <p className="celsius">°C</p>
-              </span>
-              <div className="current-comment-container">
-                <small className="today-comment">{weather.description}</small>
+                        <h3 className="today-temperature">
+                          {Math.round(weather.temperature)}
+                        </h3>
+                        <span className="temperature-unit">
+                          <p className="celsius">°C</p>
+                        </span>
+                        <div className="current-comment-container">
+                          <small className="today-comment text-capitalize">
+                            {weather.description}
+                          </small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div id="today-parameters" className="col-7">
+                    <ul className="list-group list-group-horizontal">
+                      <li className="list-group-item">
+                        <FontAwesomeIcon
+                          icon={faTemperatureLow}
+                          className="param-icons"
+                        />
+                        Low
+                      </li>
+                      <li className="list-group-item current-low-temperature">
+                        {Math.round(weather.low)}°C
+                      </li>
+                    </ul>
+                    <ul className="list-group list-group-horizontal-sm">
+                      <li className="list-group-item">
+                        <FontAwesomeIcon
+                          icon={faTint}
+                          className="param-icons"
+                        />{" "}
+                        Humidity
+                      </li>
+                      <li className="list-group-item current-humidity">
+                        {weather.humidity}%
+                      </li>
+                    </ul>
+                    <ul className="list-group list-group-horizontal-md">
+                      <li className="list-group-item">
+                        <FontAwesomeIcon
+                          icon={faWind}
+                          className="param-icons"
+                        />
+                        Wind
+                      </li>
+                      <li className="list-group-item current-wind-speed">
+                        {Math.round(weather.wind)} km/h
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
